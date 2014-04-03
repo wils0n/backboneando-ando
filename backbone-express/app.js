533c1b7f4211ed53ed4f1f9ba87b89b96f7939b8
wils0n <wiljm0.0@gmail.com>
2014-04-02 21:06:56 -0500

    backbone and expressjs integration

---
+++ backbone-express/app.js
@@ -0,0 +1,49 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+var cons = require('consolidate');
+var fs = require('fs');
+var baseData = fs.readFileSync('./base-data.json').toString();
+
+var data = JSON.parse(baseData);
+
+var app = express();
+
+// all environments
+app.engine('html', cons.swig);
+app.set('view engine', 'html');
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', cons.swig);
+//app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+// routes
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+app.get('/problems/', function(req, res){
+	res.send(data);
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
